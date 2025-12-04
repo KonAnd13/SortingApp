aac69243213918fe02475dd56a1d82a842af6823
as1992ale <57945367+as1992ale@users.noreply.github.com>
2025-12-04 21:28:06 +0300

    #7-year-power-model-sort-strategy

--- src/main/java/service/sorting/YearPowerModelSortStrategy.java
+++ src/main/java/service/sorting/YearPowerModelSortStrategy.java
@@ -1,19 +1,45 @@
 package service.sorting;
 
-import domain.car;
+import domain.Car;
 import java.util.List;
+
+import static java.util.Collections.swap;
 
 public class YearPowerModelSortStrategy implements SortStrategy<Car> {
     @Override
     public void sort(List<Car> list) {
         for (int i = 0; i < list.size() - 1; i++) {
             for (int j = 0; j < list.size() - i - 1; j++) {
-                if (list.get(j).getModel().compareTo(list.get(j + 1).getModel()) > 0) {
-                    Car temp = list.get(j);
-                    list.set(j, list.get(j + 1));
-                    list.set(j + 1, temp);
+                Car car1 = list.get(j);
+                Car car2 = list.get(j+1);
+
+                if (car1.getYear() != car2.getYear()) {
+                    if(car1.getYear() > car2.getYear()){
+                        swap(list,j);
+                    }
+                    continue;
+
                 }
+                if (car1.getPower() !=car2.getPower()){
+                    if(car1.getPower() > car2.getPower()){
+                        swap(list,j);
+                    }
+                    continue;
+
+                }
+                if (car1.getModel() !=car2.getModel()){
+                    if(car1.getModel() > car2.getModel()){
+                        swap(list,j);
+                    }
+                    continue;
+
             }
         }
+    }
+
+    private void swap(List<Car> list, int index){
+        Car temp = list.get(index);
+        list.set(index, list.get(list.size() + 1));
+        list.set(index + 1, temp);
     }
 }
