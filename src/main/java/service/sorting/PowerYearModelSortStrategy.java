7e61d093c0e8db9bb0c3390adb735951d758784d
kkonst40 <161722238+kkonst40@users.noreply.github.com>
2025-12-04 21:43:40 +0300

    #6 PowerYearModelSortStrategy

--- src/main/java/service/sorting/PowerYearModelSortStrategy.java
+++ src/main/java/service/sorting/PowerYearModelSortStrategy.java
@@ -1,23 +1,19 @@
 package service.sorting;
 
-import java.util.Comparator;
 import java.util.List;
-import domain.car;
+import domain.Car;
 
 
 public class PowerYearModelSortStrategy implements SortStrategy<Car> {
+    @Override
     public void sort(List<Car> cars) {
-        Comparator carsComparator = Comparator.comparingInt(Car::getPower)
-                .thenComparingInt(Car::getYear)
-                .thenComparing(Car::getModel);
-
         int n = cars.size();
         boolean swapped;
 
         for (int i = 0; i < n - 1; i++) {
             swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
-                if (carsComparator.compare(cars.get(j), cars.get(j + 1)) > 0) {
+                if (compare(cars.get(j), cars.get(j + 1)) > 0) {
                     Car temp = cars.get(j);
                     cars.set(j, cars.get(j + 1));
                     cars.set(j + 1, temp);
@@ -27,5 +23,20 @@ public class PowerYearModelSortStrategy implements SortStrategy<Car> {
             if (!swapped)
                 break;
         }
+    }
+
+    private int compare(Car car1, Car car2) {
+        int powerCompare = Integer.compare(car1.getPower(), car2.getPower());
+        if (powerCompare != 0) {
+            return powerCompare;
+        }
+
+        int yearCompare = Integer.compare(car1.getYear(), car2.getYear());
+        if (yearCompare != 0) {
+            return yearCompare;
+        }
+
+        int modelCompare = car1.getModel().compareTo(car2.getModel());
+        return modelCompare;
     }
 }
